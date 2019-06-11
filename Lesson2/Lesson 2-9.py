78db0dfce1549fff7f7402380207145f00148848
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-11 11:19:16 +0300

    Delete Lesson 2-9.py

--- Lesson2/Lesson 2-9.py
+++
@@ -1,9 +0,0 @@
-if __name__=='__main__':
-    print('Enter n:')
-    n=int(input())
-    l = [1, 1]
-    for i in range(2,n):
-        l.append(l[i-2]+l[i-1])
-    print('Fibonacci numbers: ',l)
-
-
