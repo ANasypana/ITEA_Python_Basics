b7a07db22b5df1a8e622ef6e9d78b27d9c9d2625
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-11 11:14:50 +0300

    Delete Lesson1-5.py

--- Lesson1/Lesson1-5.py
+++
@@ -1,9 +0,0 @@
-if __name__=='__main__':
-    print('Enter number 1:')
-    a=str(input())
-    print('Enter number 2:')
-    b=str(input())
-    a=a[-1]
-    b=b[-1]
-    c=int(a)+int(b)
-    print('Sum of the last digits: ', c)
