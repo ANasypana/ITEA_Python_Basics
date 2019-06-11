240ca164941a69c430498befe9daa325b7be657c
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-11 11:15:02 +0300

    Delete Lesson1-6.py

--- Lesson1/Lesson1-6.py
+++
@@ -1,8 +0,0 @@
-if __name__=='__main__':
-    print('Enter temperature values in Kelvin:')
-    K=float(input())
-    if K<0:
-        print('It is not correct')
-    else:
-        C=K-273.15
-        print(round(C),' C')
